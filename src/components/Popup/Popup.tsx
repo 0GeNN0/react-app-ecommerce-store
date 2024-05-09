d34d5a10c53774974cda1d5998724002870ef0c8
Omar Elsayed <omar.elgnn13@gmail.com>
2024-05-09 12:37:26 +0300

    wrapped the popup with an outer element instead of portal div

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -18,28 +18,30 @@ function Popup({ isModalOpen, toggleModal, isSpinning }: PopupProps) {
   }, [isModalOpen]);
 
   return createPortal(
-    <div className="pop-up">
-      <div className="heading-message">
-        <h2>
-          {isSpinning
-            ? "Completing Ordering Process..."
-            : `Process Completed 😍`}
-        </h2>
-        {isSpinning && <p>Please Wait</p>}
-      </div>
-      <div className="spinner">
-        {isSpinning ? (
-          <span className="circle">
-            <span className="inner-circle"></span>
-          </span>
-        ) : (
-          <span className="signs">✔ ✔ ✔</span>
-        )}
-      </div>
-      <div className="btn">
-        <button className="done" onClick={toggleModal} disabled={isSpinning}>
-          Done!
-        </button>
+    <div className='modal'>
+      <div className="pop-up">
+        <div className="heading-message">
+          <h2>
+            {isSpinning
+              ? "Completing Ordering Process..."
+              : `Process Completed 😍`}
+          </h2>
+          {isSpinning && <p>Please Wait</p>}
+        </div>
+        <div className="spinner">
+          {isSpinning ? (
+            <span className="circle">
+              <span className="inner-circle"></span>
+            </span>
+          ) : (
+            <span className="signs">✔ ✔ ✔</span>
+          )}
+        </div>
+        <div className="btn">
+          <button className="done" onClick={toggleModal} disabled={isSpinning}>
+            Done!
+          </button>
+        </div>
       </div>
     </div>,
     document.body

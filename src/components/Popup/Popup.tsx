81897567776e9621d326377a2f1888bdd5dc05ce
omar-muhammad <omarelsayedmuhammad@gmail.com>
2024-05-11 06:20:24 +0300

    tried to solve a fake bug :)

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,37 +1,37 @@
 // Libs
-import { createPortal } from "react-dom";
+import { createPortal } from 'react-dom';
 
 // Types
-import { PopupProps } from "./popup.types";
+import { PopupProps } from './popup.types';
 
 // Styles
-import "./popup.scss";
+import './popup.scss';
 
 function Popup({ isModalOpen, toggleModal, isSpinning }: PopupProps) {
   const toggleClass = isModalOpen ? 'open' : '';
 
   return createPortal(
     <div className={`modal ${toggleClass}`}>
-      <div className="pop-up">
-        <div className="heading-message">
+      <div className='pop-up'>
+        <div className='heading-message'>
           <h2>
             {isSpinning
-              ? "Completing Ordering Process..."
+              ? 'Completing Ordering Process...'
               : `Process Completed 😍`}
           </h2>
           {isSpinning && <p>Please Wait</p>}
         </div>
-        <div className="spinner">
+        <div className='spinner'>
           {isSpinning ? (
-            <span className="circle">
-              <span className="inner-circle"></span>
+            <span className='circle'>
+              <span className='inner-circle'></span>
             </span>
           ) : (
-            <span className="signs">✔ ✔ ✔</span>
+            <span className='signs'>✔ ✔ ✔</span>
           )}
         </div>
-        <div className="btn">
-          <button className="done" onClick={toggleModal} disabled={isSpinning}>
+        <div className='btn'>
+          <button className='done' onClick={toggleModal} disabled={isSpinning}>
             Done!
           </button>
         </div>
